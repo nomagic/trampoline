da6f050e16d2a796acbcbc639b0b52a5b78f3739
'thurston@nomagic' <thurston@192.168.1.70>
2015-12-21 00:09:20 -0800

    Initial version Beauty is the art of simplicity

--- src/java/com/nomagicsoftware/functional/TailCall.java
+++ src/java/com/nomagicsoftware/functional/TailCall.java
@@ -10,4 +10,46 @@ package com.nomagicsoftware.functional;
 public interface TailCall<T>
 {
     TailCall<T> call();
+    
+    default T value()
+    {
+        return null; //could throw  Exception here I guess
+    }
+    
+    default boolean finished()
+    {
+        return false;
+    }
+    
+    static <R> TailCall<R> terminate(final R value)
+    {
+        return new TailCall<R>()
+        {
+
+            @Override
+            public R value()
+            {
+                return value;
+            }
+
+            @Override
+            public boolean finished()
+            {
+                return true;
+            }
+            
+            @Override
+            public TailCall<R> call()
+            {
+                return this;
+            }
+        };
+    }
+    
+    static <R> R trampoline(TailCall<R> function)
+    {
+        for(; ! function.finished(); function = function.call())
+        {}    
+        return function.value();
+    }
 }
